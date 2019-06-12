06c57c8ee865bb2898cd72979aa49b00c612ce41
Nairi <nairi.haroutiounian@gmail.com>
2019-06-12 22:21:37 +0400

    Rename, change response type

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var http = require("http");
 const PORT = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-
-   response.end('Hello World\n');
+   response.writeHead(200, {'Content-Type': 'application/json'});
+   response.end(JSON.stringify({ message: 'Hello from Server 2' }));
 }).listen(PORT);
 
 console.log(`Server running at http://127.0.0.1:${PORT}`);
